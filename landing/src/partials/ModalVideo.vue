9510ed4ec9d21f3c26659981cce2ccef7ed970ed
anramosp <anramosp.ext@acciona.com>
2025-05-24 16:58:13 +0200

    update

--- landing/src/partials/ModalVideo.vue
+++ landing/src/partials/ModalVideo.vue
@@ -1,10 +1,18 @@
 <script setup>
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
 
 const props = defineProps({
   video: String,
   videoWidth: Number,
   videoHeight: Number,
+});
+
+const videoRef = ref(null);
+
+onMounted(() => {
+  if (videoRef.value) {
+    videoRef.value.playbackRate = 1.8; // velocidad 1.5x
+  }
 });
 </script>
 
@@ -12,6 +20,7 @@ const props = defineProps({
   <div class="relative">
     <!-- Video autoplay visible directamente -->
     <video
+      ref="videoRef"
       class="rounded-2xl w-full h-auto"
       :width="props.videoWidth"
       :height="props.videoHeight"
